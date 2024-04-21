dbcbc460ee7dcdc77d60d93d3c670b8731709498
Drvw <ShellsCode@protonmail.com>
2024-04-20 22:52:42 -0300

    Department entity class

---
+++ src/Application/Program.java
@@ -0,0 +1,14 @@
+package Application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department objT = new Department(1, "Books");
+		System.out.println(objT);
+
+	}
+
+}
